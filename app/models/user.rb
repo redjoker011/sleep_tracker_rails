0c2bf3454a8683239ec7a9ad3f9495aae95a0eee
Peter John Alvarado <redjoker011@gmail.com>
2023-04-07 00:17:26 +0800

    feat: add association for sleep logs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@
 #
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  has_many :sleep_logs
+
+  # Task
+  # Seed Users
+  # Allow Users to log sleep time and wake-up time
+  # Allow a User to follow/unfollow another User
+  #   - Create api for user lists
+  # Allow to see sleep record over past week, ordered by length of sleep
 end
